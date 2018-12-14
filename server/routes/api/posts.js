af82593fe540f3149ef60d9a707d920088da70e7
Matthieu Brouillard <mbrouilla@gmail.com>
2018-12-14 15:51:03 +0100

    Add get, delete, post API

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,11 +1,37 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
-// GET Posts
-router.get('/', (req, res) => {
-    res.send('hello');
-})
+// GET
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// POST
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb://abc123:abc123@ds121118.mlab.com:21118/venm-stack",
+    { useNewUrlParser: true }
+  );
+  return client.db("venm-stack").collection("posts");
+}
 
 module.exports = router;
